2f808326ab1b450fc4c34ce44a3bc3cf7409ee0b
kavi-thagadoor <kaviarasan.m@thagadoor.org>
2025-01-24 20:56:21 +0530

    test admon ad init done success

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 declare let window: any; 
+import { AdMob, AdOptions, BannerAdSize } from '@capacitor-community/admob';
+
 
 @Component({
   selector: 'app-home',
@@ -22,4 +24,32 @@ export class HomePage {
       console.error('Flashlight plugin not available.');
     }
   }
+
+
+  constructor() {}
+
+  async showAd() {
+    const adOptions: AdOptions = {
+      adId: 'ca-app-pub-3940256099942544/6300978111', // Test Banner Ad Unit ID
+      isTesting: true, // Ensure test ads are used
+      // To control alignment, you can use additional styles (explained below).
+    };
+  
+    try {
+      await AdMob.initialize(); // Initialize AdMob
+      await AdMob.showBanner(adOptions); // Show the ad
+      console.log('Ad displayed at the top!');
+    } catch (error) {
+      console.error('Error showing AdMob banner:', error);
+    }
+  }
+
+  async hideAd() {
+    try {
+      await AdMob.hideBanner(); // Hide the ad
+    } catch (error) {
+      console.error('Error hiding AdMob banner:', error);
+    }
+  }
+
 }
